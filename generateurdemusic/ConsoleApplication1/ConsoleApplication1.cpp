b7c2390b70c2457d7ba1f73ec4e6fd73054f3df8
cocgabriel <gabriel.cochet@ynov.com>
2025-01-24 09:41:24 +0100

    sdl

--- generateurdemusic/ConsoleApplication1/ConsoleApplication1.cpp
+++ generateurdemusic/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,43 +1,26 @@
+#include <SDL.h>
 #include <iostream>
-#include <conio.h>   
-#include <windows.h> 
-#include <map>       
-#include <string>    
 
-int main() {
-    // Map des touches vers les fichiers de notes
-    std::map<char, std::wstring> keyToNote = {
-        {'a', L"C.wav"},  // 'a' -> note C
-        {'s', L"D.wav"},  // 's' -> note D
-        {'d', L"E.wav"},  // 'd' -> note E
-        {'f', L"F.wav"},  // 'f' -> note F
-        {'g', L"G.wav"},  // 'g' -> note G
-        {'h', L"A.wav"},  // 'h' -> note A
-        {'j', L"B.wav"}   // 'j' -> note B
-    };
-
-    std::cout << "Appuyez sur les touches (a, s, d, f, g, h, j) pour jouer des notes. Appuyez sur 'q' pour quitter.\n";
-
-    while (true) {
-        char key = _getch(); // Attend qu'une touche soit pressée
-        if (key == 'q') {
-            std::cout << "Fermeture du programme.\n";
-            break; // Quitte la boucle si 'q' est pressé
-        }
-
-        if (keyToNote.find(key) != keyToNote.end()) {
-            // Joue le son associé à la touche
-            std::wstring soundFile = keyToNote[key];
-            PlaySound(soundFile.c_str(), NULL, SND_FILENAME | SND_ASYNC);
-            std::cout << "Note jouée : " << key << "\n";
-        }
-        else {
-            std::cout << "Touche invalide. Appuyez sur une touche valide (a, s, d, f, g, h, j).\n";
-        }
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
     }
 
+    SDL_Window* window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000); // Wait for 3 seconds
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
+
 
 // Classe de base pour un instrument
 class Instrument {
